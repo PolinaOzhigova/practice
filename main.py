46f6d1b959ea98e198ed52b28b6195a7106ca9ef
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-06-21 11:23:11 +0800

    make fastapi

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hi!!!')
+from pydantic import EmailStr
+from fastapi import FastAPI
+api = FastAPI()
+
+@api.get("/")
+async def index():
+    return {"Message": "Hello everyone"}
+
+@api.get("/hello")
+async def index(user_name: str | None = None):
+    if user_name:
+        return {"Message": f"Hello {user_name}"}
+    else:
+        return {"Message": f"Hello anon"}
+
+@api.post("/create_user")
+async def create_user(user_name: str, user_mail: EmailStr):
+    return {"user_name": user_name, "user_mail": user_mail}
