2a8e6508e368e6adf6d1d84b8320aead17f3ca42
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-06-21 14:38:32 +0800

    update main.py

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ async def index(user_name: str | None = None):
 @api.post("/create_user")
 async def create_user(user_name: str, user_mail: EmailStr):
     return {"user_name": user_name, "user_mail": user_mail}
+
+class UserBase(BaseModel):
+    name:str
+    email: EmailStr
+class UserOut(UserBase):
+    pass
+class UserIn(UserBase):
+    password: str
+
+@api.post("/create_user_from_model", response_model=UserOut)
+async def create_user_from_model(user: UserIn):
+    return user 
